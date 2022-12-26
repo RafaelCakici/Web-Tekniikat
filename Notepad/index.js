66ad75b7e94dcbf6a150b321080622a887f166f9
Rafael_Cakici <n2cara00@students.oamk.fi>
2022-12-26 19:12:41 +0200

    Borders around text

--- Notepad/index.js
+++ Notepad/index.js
@@ -10,3 +10,11 @@ out2.innerHTML = areaone.value;
 }
 
 btn.addEventListener('click',fun1);
+
+function addBorder() {
+    // Get the text element
+    let text = document.getElementById("output2");
+
+    // Add a border to the text element
+    text.style.border = "1px solid black";
+  }
